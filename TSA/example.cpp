b4e205d81e944d8ed1200ec3f5c7b301933ac7fe
Alison Chaiken <alison@she-devel.com>
2025-05-02 17:12:36 -0400

    move function definitions out of example hdr

---
+++ TSA/example.cpp
@@ -0,0 +1,22 @@
+#include "example.hpp"
+
+void BankAccount::depositImpl(int amount) {
+  balance += amount; // WARNING! Cannot write balance without locking mu.
+}
+
+void BankAccount::withdrawImpl(int amount) REQUIRES(mu) {
+  balance -= amount; // OK. Caller must have locked mu.
+}
+
+void BankAccount::withdraw(int amount) {
+  mu.Lock();
+  withdrawImpl(amount); // OK.  We've locked mu.
+} // WARNING!  Failed to unlock mu.
+
+void BankAccount::transferFrom(BankAccount &b, int amount) {
+  mu.Lock();
+  b.withdrawImpl(
+      amount); // WARNING!  Calling withdrawImpl() requires locking b.mu.
+  depositImpl(amount); // OK.  depositImpl() has no requirements.
+  mu.Unlock();
+}
--- TSA/example.hpp
+++ TSA/example.hpp
@@ -1,29 +1,16 @@
-#include "mutex.hh"
+#include "mutex.hpp"
 
 class BankAccount {
 private:
   Mutex mu;
   int balance GUARDED_BY(mu);
 
-  void depositImpl(int amount) {
-    balance += amount; // WARNING! Cannot write balance without locking mu.
-  }
+  void depositImpl(int amount);
 
-  void withdrawImpl(int amount) REQUIRES(mu) {
-    balance -= amount; // OK. Caller must have locked mu.
-  }
+  void withdrawImpl(int amount);
 
 public:
-  void withdraw(int amount) {
-    mu.Lock();
-    withdrawImpl(amount); // OK.  We've locked mu.
-  } // WARNING!  Failed to unlock mu.
+  void withdraw(int amount);
 
-  void transferFrom(BankAccount &b, int amount) {
-    mu.Lock();
-    b.withdrawImpl(
-        amount); // WARNING!  Calling withdrawImpl() requires locking b.mu.
-    depositImpl(amount); // OK.  depositImpl() has no requirements.
-    mu.Unlock();
-  }
+  void transferFrom(BankAccount &b, int amount);
 };

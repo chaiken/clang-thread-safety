e720f7028bc90e15607ead52081451dfe0989f5e
Alison Chaiken <alison@she-devel.com>
2025-05-07 22:22:12 -0400

    thread-safety: create a getter and remove GUARDED_BY
    There is no obvious way for a data member to be make as guarded by a mutex for
    writes and a shared_mutex for reads.  Removing GUARDED_BY is regrettable, but
    marking the only writer functions as REQUIRES(mu_) should provide adequate
    protection.  Make the shared_mutex mutable so that const functions can change it
    and only it.

--- TSA/example.hpp
+++ TSA/example.hpp
@@ -4,13 +4,25 @@ namespace tsa_example {
 
 class BankAccount {
 private:
+  // Make the SharedMutex mutable so that const methods can modify it, but not
+  // mu_ or balance_.
+  mutable tsa::SharedMutex smu_;
   tsa::Mutex mu_;
-  std::size_t balance_ GUARDED_BY(mu_);
+  // Remove GUARDED_BY from balance_ since otherwise reading it requires a
+  // writer lock.
+  std::size_t balance_ = 0;
 
   void depositImpl(const std::size_t amount) REQUIRES(mu_);
   void withdrawImpl(const std::size_t amount) REQUIRES(mu_);
 
 public:
+  // Create a getter which takes a reader lock.
+  std::size_t balance() const {
+    tsa::shared_lock_t lockt;
+    tsa::SharedMutexLocker sml(&smu_, lockt);
+    return balance_;
+  }
+  // TODO: fail if amount > balance_.
   void withdraw(const std::size_t amount);
   void deposit(const std::size_t amount);
   void transferFrom(BankAccount &b, const std::size_t amount);
